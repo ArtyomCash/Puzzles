fa413b5f911044ace50479eaf7a589c94b07f6d1
Artyom <cash.kin.art@gmail.com>
2022-07-08 00:30:56 +0300

    square

---
+++ js/puzzlesArray.js
@@ -0,0 +1,14 @@
+// firstArea.innerHTML += `<div class="player" style="left: ${player.x}px; top: ${player.y}px;"></div>`;
+
+
+// function newArea () {
+//     let newArea = document.querySelectorAll('.new-area');
+//     newArea.innerHTML += `<div class="firstArea"></div>`;
+    // newArea.style.width = '100px';
+    // newArea.style.height = '100px';
+    // newArea.style.backgroundColor = 'green';
+// }
+// newArea();
+
+let firstArea = document.getElementsByClassName('first-area');
+
